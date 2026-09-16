0fc1a5c5760c59a6e59f748206dd2c356e0c5a16
A1DR1C <roycearokiaraj@gmail.com>
2026-09-16 17:49:26 -0400

    homework template

---
+++ HW01.cpp
@@ -0,0 +1,80 @@
+/*
+
+HW01:
+Author: Royce Arokia-Raj
+Cite:
+
+1. Run the code single threaded
+2. benchmark the code multi threaded writing to *pcount every time (Slower)
+3. benchmark the code multi threaded with a pool of 2 threads (4 optional)
+4. Now you write partitioning. Divide not into 2 but into chunks
+
+Put the table of results in the comment.
+
+
+*/
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <cmath>
+using namespace std;
+
+// example n = 1000000001
+// O(sqrt n)  omega(1)
+inline bool isPrime(uint64_t n) {
+    uint64_t lim = sqrt(n);
+    for (uint64_t i = 3; i <= lim; i+=2) {
+      if (n % i == 0) // if n is evenly divisible by i
+        return false;
+    }
+    return true;
+}
+// 3, 5, 7, 9, 11, 13, 15, 17, ... 1001, 1'000'000'000
+uint64_t countPrimes(uint64_t n) { //O(n sqrt(n))
+    uint64_t count = 1;
+    for (int i = 3; i <= n; i+=2) //O(n)
+      if (isPrime(i)) //O(sqrt(n))
+        count++;
+    return count;
+}
+// n = 10^9
+// 2-1K, 1K+1-2K, 2K+1-3K, 
+
+void countPrimesMultithreaded(uint64_t a, uint64_t b, uint64_t* pcount) {
+//    uint64_t count = 1;
+    *pcount = (a == 2 ? 1 : 0);
+    a |= 1; // 10101010101010101010101010100001
+  // THE ABOVE GUARANTEES THAT a IS odd 
+//    if (a % 2 == 0)
+//      a++;
+    for (int i = a; i <= b; i+=2) //O(n)
+      if (isPrime(i)) //O(sqrt(n))
+        (*pcount)++;
+}
+
+
+// do the counting in a register, do not write to memory
+void countPrimesMultithreaded2(uint64_t a, uint64_t b, uint64_t* pcount) {
+    uint64_t count = (a == 2 ? 1 : 0);
+    a |= 1; // 10101010101010101010101010100001
+  // THE ABOVE GUARANTEES THAT a IS odd 
+    for (int i = a; i <= b; i+=2) //O(n)
+      if (isPrime(i)) //O(sqrt(n))
+        count++;
+    *pcount = count;
+}
+
+int main(int argc, char* argv[]) {
+    uint64_t n = atol(argv[1]);
+    uint64_t chunkSize = 1024*1024;
+    uint64_t current = 2;
+//    std::cout << countPrimes(n) << '\n';
+    uint64_t count1 = 0, count2 = 0;
+    thread t1(countPrimesMultithreaded2, 2, n/2, &count1);
+    thread t2(countPrimesMultithreaded2, n/2+1, n, &count2);
+    t1.join();
+    t2.join();
+    uint64_t count = count1 + count2;
+    cout << count << '\n';
+
+}
